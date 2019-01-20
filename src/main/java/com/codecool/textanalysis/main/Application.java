51d60d15050e11fbf3167061f3aa65a7443a1d86
PatrykMandrak <patrykmandrak123@gmail.com>
2019-01-20 20:53:43 +0100

    Add main method

--- src/main/java/com/codecool/textanalysis/main/Application.java
+++ src/main/java/com/codecool/textanalysis/main/Application.java
@@ -1,5 +1,17 @@
+package com.codecool.textanalysis.main;
+
+import com.codecool.textanalysis.controllers.RootController;
+import com.codecool.textanalysis.views.RootView;
+
+import java.io.PrintStream;
+
 public class Application {
 
     public static void main(String... args) {
+
+        PrintStream printStream = new PrintStream(System.out);
+        RootView rootView = new RootView(printStream);
+        RootController rootController = new RootController(args, rootView);
+        rootController.start();
     }
 }
