baa8c345025477fcb0089d0badfeee2fe9f39eaf
PatrykMandrak <patrykmandrak123@gmail.com>
2019-01-20 21:06:01 +0100

    Implement taking all necessary informations from services

--- src/main/java/com/codecool/textanalysis/controllers/RootController.java
+++ src/main/java/com/codecool/textanalysis/controllers/RootController.java
@@ -42,4 +42,78 @@ public class RootController {
             throw new IllegalArgumentException(filepath + " file does not exist!");
         }
     }
+
+    private void handleAnalysisAndDisplayResults(String filename, IterableText iterableText) {
+        showFileName(filename);
+        showAllCharactersCountInFile(iterableText);
+        showAllWordsCountInFile(iterableText);
+        showDifferentWordsUsedQuantity(iterableText);
+        showMostUsedWords(iterableText);
+        showLoveCount(iterableText);
+        showHateCount(iterableText);
+        showMusicCount(iterableText);
+        showVowelsPercentage(iterableText);
+        showAToERatio(iterableText);
+        showPercentageOfAllLetters(iterableText);
+    }
+
+    private void showFileName(String filename) {
+        rootView.displayFileName(filename);
+    }
+
+    private void showAllCharactersCountInFile(IterableText iterableText) {
+        rootView.displayCharCount(new StatisticalAnalysis(iterableText.charIterator()).size());
+    }
+
+    private void showAllWordsCountInFile(IterableText iterableText) {
+        rootView.displayWordCount(new StatisticalAnalysis(iterableText.wordIterator()).size());
+    }
+
+    private void showDifferentWordsUsedQuantity(IterableText iterableText) {
+        rootView.displayDictSize(new StatisticalAnalysis(iterableText.wordIterator()).dictionarySize());
+    }
+
+    private void showMostUsedWords(IterableText iterableText) {
+        double onePercent = 0.01;
+        Double onePercentOfAllWords = onePercent * new StatisticalAnalysis(iterableText.wordIterator()).size();
+        rootView.displayMostUsedWords(new StatisticalAnalysis(iterableText.wordIterator())
+                .occurMoreThan(onePercentOfAllWords.intValue()));
+    }
+
+    private void showLoveCount(IterableText iterableText) {
+        rootView.displayLoveWordCount(new StatisticalAnalysis(iterableText.wordIterator()).countOf("love"));
+    }
+
+    private void showHateCount(IterableText iterableText) {
+        rootView.displayHateWordCount(new StatisticalAnalysis(iterableText.wordIterator()).countOf("hate"));
+    }
+
+    private void showMusicCount(IterableText iterableText) {
+        rootView.displayMusicWordCount(new StatisticalAnalysis(iterableText.wordIterator()).countOf("music"));
+    }
+
+    private void showVowelsPercentage(IterableText iterableText) {
+        double vowelsCount = new StatisticalAnalysis(iterableText.charIterator())
+                .countOf("a", "e", "i", "o", "u");
+        double allCharacterQuantity = new StatisticalAnalysis(iterableText.charIterator()).size();
+        rootView.displayVowelsPercentage((vowelsCount / allCharacterQuantity) * 100);
+    }
+
+    private void showAToERatio(IterableText iterableText) {
+        double aCount = new StatisticalAnalysis(iterableText.charIterator()).countOf("a");
+        double eCount = new StatisticalAnalysis(iterableText.charIterator()).countOf("e");
+        rootView.displayAToERatio(aCount / eCount);
+    }
+
+    private void showPercentageOfAllLetters(IterableText iterableText) {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        double allCharacterQuantity = new StatisticalAnalysis(iterableText.charIterator()).size();
+        Map<String, Double> map = new HashMap<>();
+        for (char ch : alphabet) {
+            int characterCount = new StatisticalAnalysis(iterableText.charIterator()).countOf(Character.toString(ch));
+            map.put(Character.toString(ch), (characterCount / allCharacterQuantity) * 100);
+        }
+        rootView.displayPercentageOfAllLetters(map);
+    }
+
 }
